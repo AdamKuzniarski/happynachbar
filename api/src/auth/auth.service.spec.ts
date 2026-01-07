d0e89d329b47c9649087c8a7761f7e5663af84a7
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-01-07 10:52:35 +0100

    add basic test for auth controller, sevice

--- api/src/auth/auth.service.spec.ts
+++ api/src/auth/auth.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const prismaMock = {
+    user: {
+      create: jest.fn(),
+      findUnique: jest.fn(),
+      update: jest.fn(),
+    },
+  };
+
+  const jwtMock = {
+    singAsync: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+        { provide: JwtService, useValue: jwtMock },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- api/test/global-teardown.ts
+++ api/test/global-teardown.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as dotenv from 'dotenv';
 import { Client } from 'pg';
+import * as fs from 'node:fs';
 
 export default async function globalTeardown() {
   dotenv.config({ path: path.resolve(__dirname, '../.env.test') });
@@ -8,10 +9,9 @@ export default async function globalTeardown() {
   const baseUrl = process.env.DATABASE_URL;
   if (!baseUrl) throw new Error('Missing DATABASE_URL in .env.test');
 
-  const fs = await import('fs');
   const schemaFile = path.resolve(__dirname, './.test-schema');
   const schema = fs.existsSync(schemaFile)
-    ? fs.readFile(schemaFile, 'utf8')
+    ? fs.readFileSync(schemaFile, 'utf8').trim()
     : null;
 
   if (!schema) return;

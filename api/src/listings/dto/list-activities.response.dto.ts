c56f7b2e7c2d639205ff11d92f19a882203f5118
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-01-09 14:54:35 +0100

    add activity-list DTO

---
+++ api/src/listings/dto/list-activities.response.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ActivityCardDto } from './activity-card.dto';
+
+export class ListActivitiesResponseDto {
+  @ApiProperty({ type: () => [ActivityCardDto] })
+  items!: ActivityCardDto[];
+
+  @ApiProperty({
+    example: null,
+    nullable: true,
+    description: 'If null => no more pages',
+  })
+  nextCursor!: string | null;
+}

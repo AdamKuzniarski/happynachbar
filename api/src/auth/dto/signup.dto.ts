4e5847ac0fdea7ef3fc8a4f27d99d71567a086d3
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-01-13 10:43:00 +0100

    update schema.prisma and signupDTO

--- api/src/auth/dto/signup.dto.ts
+++ api/src/auth/dto/signup.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SignupDto {
@@ -8,9 +8,20 @@ export class SignupDto {
   @IsString()
   @MinLength(8)
   @ApiProperty({
-    example: 'baby roth julia uuasda',
+    example: 'baby-roth-julia',
     minLength: 8,
     description: 'at least 8 characters',
   })
   password: string;
+
+  @ApiProperty({
+    example: 'julia@shark.de',
+    minLength: 2,
+    maxLength: 50,
+    description: 'at least 2 and less than 50 characters',
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
+  displayName?: string;
 }

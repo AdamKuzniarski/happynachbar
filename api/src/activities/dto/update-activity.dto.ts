053ac97c0633d608bab4eda9cc7ffcf9e2a8aeba
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-01-14 14:23:18 +0100

    change update-activity.dto to partial type

--- api/src/activities/dto/update-activity.dto.ts
+++ api/src/activities/dto/update-activity.dto.ts
@@ -1,44 +1,4 @@
-import {
-  ArrayMaxSize,
-  IsArray,
-  IsDateString,
-  IsEnum,
-  IsOptional,
-  IsString,
-  IsUrl,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
-import { ActivityCategory } from './activity-category.enum';
+import { PartialType } from '@nestjs/swagger';
+import { CreateActivityDto } from './create-activity.dto';
 
-export class UpdateActivityDto {
-  @IsOptional()
-  @IsString()
-  @MinLength(3)
-  @MaxLength(120)
-  title?: string;
-
-  @IsOptional()
-  @IsString()
-  @MaxLength(2000)
-  description?: string;
-
-  @IsOptional()
-  @IsEnum(ActivityCategory)
-  category?: ActivityCategory;
-
-  @IsOptional()
-  @Matches(/^\d{5}$/)
-  plz?: string;
-
-  @IsOptional()
-  @IsDateString()
-  startAt?: string;
-
-  @IsOptional()
-  @IsArray()
-  @ArrayMaxSize(5)
-  @IsUrl({}, { each: true })
-  imageUrls?: string[];
-}
+export class UpdateActivityDto extends PartialType(CreateActivityDto) {}

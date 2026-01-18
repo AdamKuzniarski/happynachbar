10e891987b93298afd7a144652c46801a015b8ec
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-01-18 13:15:02 +0100

    expand users.conroller

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Patch, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/jwt.guard';
 import { UsersService } from './users.service';
 import { UpdateMeDto } from './dto/update-me.dto';
@@ -10,6 +10,11 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 @ApiTags('users')
 export class UsersController {
   constructor(private users: UsersService) {}
+
+  @Get('me')
+  getMe(@Req() req: any) {
+    return this.users.getMe(req.user.userId);
+  }
 
   @Patch('me')
   updateMe(@Req() req: any, @Body() dto: UpdateMeDto) {

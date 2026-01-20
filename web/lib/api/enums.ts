9f2057103f798da9251616c2aff621d0cc33f45e
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-01-20 13:09:24 +0100

    add buildQuery eums and types in lib

---
+++ web/lib/api/enums.ts
@@ -0,0 +1,15 @@
+export enum ActivityCategory {
+  OUTDOOR = "OUTDOOR",
+  SPORT = "SPORT",
+  SOCIAL = "SOCIAL",
+  INDOOR = "INDOOR",
+  OTHER = "OTHER",
+}
+
+export const ACTIVITY_CATEGORIES: ActivityCategory[] = [
+  ActivityCategory.OUTDOOR,
+  ActivityCategory.SPORT,
+  ActivityCategory.SOCIAL,
+  ActivityCategory.INDOOR,
+  ActivityCategory.OTHER,
+];
